81270002e81ca979d5ab52fe07df86d13254e8e6
jordanforbes <jordforb@gmail.com>
2020-07-21 20:29:13 -0400

    added chart

--- src/App.js
+++ src/App.js
@@ -4,17 +4,30 @@ import styles from './App.module.css';
 import { fetchData } from './api'
 
 class App extends React.Component{
+    state = {
+        data: {},
+        country: '',
+    }
 
     async componentDidMount(){
         const data = await fetchData();
 
-        console.log(data);
+        this.setState({ data: data })
     }
+
+    handleCountryChange = async (country) =>{
+        const fetchedData = await fetchData(country);
+        console.log(country);
+        //fetch the data
+        //set the date
+    }
+
     render(){
+        const { data } = this.state
         return(
             <div className={styles.container}>
-                <Cards />
-                <CountryPicker />
+                <Cards data = {data} />
+                <CountryPicker handleCountryChange={this.handleCountryChange} />
                 <Chart />
             </div>
         )
